163cd0053c7d42741c926f9f2f25031d2acf5e24
Izam Mohammed <izamdeveloper1@gmail.com>
2023-12-10 23:31:42 +0530

    solved a stupid mistake of wrong project name

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format="[%(asctime)s]: %(message)s:")
 
 
-project_name = "loanPredction"
+project_name = "loanPrediction"
 
 
 list_of_files = [
